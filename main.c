bf03315a6c44e20e74f4340b9c4a96b73776bc7c
yaeow <matthewan370@gmail.com>
2025-09-24 18:51:40 -0300

    Mestre

--- main.c
+++ main.c
@@ -1,65 +1,93 @@
-// Requisitos funcionais
-//
-//
-// 1. Entrada de Dados: Os valores para o número de casas a serem movidas(duas para baixo, uma para a esquerda) devem ser definidos no código como variáveis ou constantes.
-//
-// 2. Lógica de Movimentação: O programa deverá implementar a lógica específica do movimento em "L" do Cavalo usando loops aninhados(um loop for e um loop while ou do - while).
-//
-// 3. Saída de Dados: O programa deverá imprimir no console a direção de cada etapa do movimento do Cavalo("Baixo", "Baixo", "Esquerda"), utilizando o comando printf.Para as outras peças as saídas devem seguir o padrão : printf("Cima\n");, printf("Baixo\n");, printf("Esquerda\n");, printf("Direita\n");.Separe o movimento do Cavalo dos movimentos anteriores com uma linha em branco.
-
 #include <stdio.h>
 
 #define TOWER_MOV 5
-#define BISHOP_MOV 5
+#define BISHOP_MOV_VERTICAL 3
+#define BISHOP_MOV_HORIZONTAL 2
 #define QUEEN_MOV 8
-
 #define HORSE_VERTICAL_MOV 2
 #define HORSE_HORIZONTAL_MOV 1
 
-int main() {
-	unsigned int i, j;
-	
-	printf("Movimento da Torre: \n");
-	for (i = 1; i <= TOWER_MOV; i++) {
-		printf("Direita\n");
+/*
+ *	@brief Move a torre para a direita em um nÃºmero especÃ­fico de casas
+ *
+ *	@param value NÃºmero de casas para mover a torre
+ */
+void move_tower(int value) {
+	if (value <= 0) return;
+
+	printf("Direita\n");
+	move_tower(value - 1);
+}
+
+/*
+ *	@brief Move o bispo para cima e direita
+ *
+ *	@param vertical Quantidade de casas na vertical
+ *	@param horizontal Quantidade de casas na horizontal
+ */
+void move_bishop(int vertical, int horizontal) {
+	if (vertical <= 0) return;
+
+	for (int i = 0; i < horizontal; i++) {
+		printf("Cima, Direita\n");
 	}
+
+	move_bishop(vertical - 1, horizontal);
+}
+
+/*
+ *	@brief Move a rainha para a esquerda em um nÃºmero especÃ­fico de casas
+ *
+ *	Esta Ã© uma funÃ§Ã£o recursiva, caso o parÃ¢metro seja menor ou igual 0 irÃ¡ interromper o loop
+ *
+ *	@param value NÃºmero de casas para mover a rainha
+ */
+void move_queen(int value) {
+	if (value < 0) return;
+
+	printf("Esquerda\n");
+	move_queen(value - 1);
+}
+
+/*
+ *	@brief Move o cavalo em L
+ *
+ *	@param vertical NÃºmero de casas na vertical
+ *	@param horizontal NÃºmero de casas na horizontal
+ */
+void move_horse(int vertical, int horizontal) {
+	for (int i = 1; i <= vertical; i++) {
+		printf("Cima\n");
+		int j = 1;
+
+		// Na segunda condiÃ§Ã£o verificamos se o valor de i Ã© estritamente
+		// igual a quantidade de casas que o cavalo percorre na vertical
+		while (j <= horizontal && i == vertical) {
+			printf("Direita\n");
+
+			j++;
+		}
+	}
+}
+
+int main() {
+	printf("Movimento da Torre: \n");
+	move_tower(TOWER_MOV);
 
 	printf("\n");
 
 	printf("Movimento do Bispo: \n");
-	i = 1;
-	while (i <= BISHOP_MOV) {
-		printf("Cima, Direita\n");
-
-		i++;
-	}
+	move_bishop(BISHOP_MOV_VERTICAL, BISHOP_MOV_HORIZONTAL);
 
 	printf("\n");
 
 	printf("Movimento da Rainha: \n");
-	i = 1;
-	do {
-		printf("Esquerda\n");
-
-		i++;
-	} while (i <= QUEEN_MOV);
+	move_queen(QUEEN_MOV);
 
 	printf("\n");
 
-	// Movimento do Cavalo: 2 casas para baixo + 1 para esquerda (loops aninhados)
 	printf("Movimento do Cavalo: \n");
-
-	// Loop para movimentos verticais (2 casas para baixo)
-	for (i = 1; i <= HORSE_VERTICAL_MOV; i++) {
-		printf("Baixo\n");
-		j = 1;
-
-		// Executa movimento horizontal (1 casa para esquerda) apenas após o 2º movimento vertical
-		while (j <= HORSE_HORIZONTAL_MOV && i == HORSE_VERTICAL_MOV) {
-			printf("Esquerda\n");
-			j++;
-		}
-	}
+	move_horse(HORSE_VERTICAL_MOV, HORSE_HORIZONTAL_MOV);
 
 	return 0;
 }
